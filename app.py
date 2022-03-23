2aaebc1493d12ae689350cc25ff9e6db8847adc2
Sumeet1616 <81152889+Sumeet1616@users.noreply.github.com>
2022-03-23 17:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def page():
             st.markdown("***Choose your genre:***")
             genre = st.radio(
                 "",
-                genre_names, index=genre_names.index("Pop"))
+                genre_names, index=genre_names.index(" Hip Hop"))
         with col1:
             st.markdown("***Choose features to customize:***")
             start_year, end_year = st.slider(
